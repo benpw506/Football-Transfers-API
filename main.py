d32c8f9033e494d3eea2906df20c600113e7fd29
benpw506 <98822094+benpw506@users.noreply.github.com>
2024-11-19 13:55:24 +0000

    Fixed Typo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins = [os.getenv("IP")],
-    allow_credientials =True, 
+    allow_credentials =True, 
     allow_methods = ["*"], 
     allow_headers=["*"]
     
